d2e9b5a2a9ce9722f3d7492f262e5f9e078257ef
red-meadow <wlad.zoloto@gmail.com>
2023-10-21 08:38:27 +0300

    Add i18n

---
+++ src/I18n.js
@@ -0,0 +1,7 @@
+export const i18n = {
+  totalUsersStart: 'Total users (start)',
+  totalUsersEnd: 'Total users (end)',
+  subs: 'Subscribers',
+  unsubs: 'Unsubscribers',
+  activity: "Activity"
+}
